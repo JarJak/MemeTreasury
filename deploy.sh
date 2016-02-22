df31d6316ebccaba055a2fe702af8f03dc6949e7
Jarek Jakubowski <egger1991@gmail.com>
2016-02-22 21:25:35 +0200

    deploy.sh to be working on windows

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set SYMFONY_ENV=loc
 
+#php composer install
 php composer update
 php bin/console doctrine:schema:update --force --env=loc
 rm -rf var/cache/*
-php bin/console server:start -q --env=loc
-php bin/codeception run
+php bin/console server:start
+#php bin/codeception run
